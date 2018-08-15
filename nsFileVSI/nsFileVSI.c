9d90e29d384dd9548e46e9b6d083ae0d29d3833c
halx99 <halx99@163.com>
2018-08-16 02:19:30 +0800

    Initial Commit, Please use VS2010SP1

---
+++ nsFileVSI/nsFileVSI.c
@@ -0,0 +1,185 @@
+// nsFileVSI.cpp : Defines the exported functions for the DLL application.
+//
+
+#include <Windows.h>
+
+#include "nsis/pluginapi.h" // nsis plugin
+
+#if defined(_DEBUG)
+#pragma comment(lib, "Version.lib")
+#pragma comment(lib, "nsis/pluginapi-x86-ansi.lib")
+#endif
+
+typedef BOOL(WINAPI *LPFN_ISWOW64PROCESS) (HANDLE, PBOOL);
+
+static
+BOOL isWOW64()
+{
+    BOOL bIsWow64 = FALSE;
+
+    //IsWow64Process is not available on all supported versions of Windows.    
+    //Use GetModuleHandle to get a handle to the DLL that contains the function    
+    //and GetProcAddress to get a pointer to the function if available.    
+
+    LPFN_ISWOW64PROCESS fnIsWow64Process = (LPFN_ISWOW64PROCESS)GetProcAddress(
+        GetModuleHandle(TEXT("kernel32")), "IsWow64Process");
+
+    if (NULL != fnIsWow64Process)
+    {
+        if (!fnIsWow64Process(GetCurrentProcess(), &bIsWow64))
+        {
+            return FALSE;
+        }
+    }
+    return bIsWow64;
+}
+
+static
+BOOL getProductVersion(LPCTSTR lpszFileName, TCHAR* pszOutputBuffer, DWORD dwOutputSize)
+{
+	DWORD dwVerHnd = 0;
+	DWORD dwVerInfoSize;
+    TCHAR szSrcfn[MAX_PATH];
+	WORD nVer[4];
+
+    lstrcpy(szSrcfn, lpszFileName);
+
+    dwVerInfoSize = GetFileVersionInfoSize(szSrcfn, &dwVerHnd);
+    if (dwVerInfoSize)
+    {
+        HANDLE hMem;
+        LPVOID lpvMem;
+        unsigned int uInfoSize = 0;
+        VS_FIXEDFILEINFO * pFileInfo;
+
+        hMem = GlobalAlloc(GMEM_MOVEABLE, dwVerInfoSize);
+        lpvMem = GlobalLock(hMem);
+        GetFileVersionInfo(szSrcfn, dwVerHnd, dwVerInfoSize, lpvMem);
+        VerQueryValue(lpvMem, (LPTSTR)_T("\\"), (void**)&pFileInfo, &uInfoSize);
+
+        
+        nVer[0] = HIWORD(pFileInfo->dwProductVersionMS);
+        nVer[1] = LOWORD(pFileInfo->dwProductVersionMS);
+        nVer[2] = HIWORD(pFileInfo->dwProductVersionLS);
+        nVer[3] = LOWORD(pFileInfo->dwProductVersionLS);
+        wsprintfA(pszOutputBuffer, _T("%d.%d.%d.%d"), nVer[0], nVer[1], nVer[2], nVer[3]);
+
+        GlobalUnlock(hMem);
+        GlobalFree(hMem);
+
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+    // To work with Unicode version of NSIS, please use TCHAR-type
+    // functions for accessing the variables and the stack.
+
+    // HWND g_hwndParent;
+    // HINSTANCE g_hInstance;
+    void __declspec(dllexport) GetProductVersion(HWND hwndParent, int string_size,
+        LPTSTR variables, stack_t **stacktop,
+        extra_parameters *extra, ...)
+    {
+		// note if you want parameters from the stack, pop them off in order.
+        // i.e. if you are called via exdll::myFunction file.dat read.txt
+        // calling popstring() the first time would give you file.dat,
+        // and the second time would give you read.txt. 
+        // you should empty the stack of your parameters, and ONLY your
+        // parameters.
+        char pszFilePath[1024];
+        char pszVersion[256] = { 0 };
+
+        EXDLL_INIT();
+        // g_hwndParent = hwndParent;
+
+
+
+
+        PopStringA(pszFilePath);
+
+        if (getProductVersion(pszFilePath, pszVersion, sizeof(pszVersion))) {
+            PushStringA(pszVersion);
+        }
+        else {
+            PushStringA("failed");
+        }
+    }
+
+
+    void __declspec(dllexport) GetSystemFileProductVersion(HWND hwndParent, int string_size,
+        LPTSTR variables, stack_t **stacktop,
+        extra_parameters *extra, ...)
+    {
+        // note if you want parameters from the stack, pop them off in order.
+        // i.e. if you are called via exdll::myFunction file.dat read.txt
+        // calling popstring() the first time would give you file.dat,
+        // and the second time would give you read.txt. 
+        // you should empty the stack of your parameters, and ONLY your
+        // parameters.
+        char fileName[256];
+        char fullPath[2048];
+        char pszVersion[256] = { 0 };
+		int arch = 0;
+		BOOL x64os = FALSE;
+		BOOL validArgs = TRUE;
+
+        EXDLL_INIT();
+        // g_hwndParent = hwndParent;
+
+        
+        PopStringA(fileName);
+        arch = popint();
+
+        GetEnvironmentVariable("WINDIR", fullPath, sizeof(fullPath));
+
+        x64os = isWOW64();
+
+        if ((x64os && arch == 0x64) || (!x64os && arch == 0x86)) {
+            lstrcatA(fullPath, "\\System32\\");
+        }
+        else if (x64os && arch == 0x86) {
+            lstrcatA(fullPath, "\\SysWOW64\\");
+        }
+        else { // Invalid
+            validArgs = FALSE;
+        }
+        if (validArgs) {
+            lstrcatA(fullPath, fileName);
+
+            if (getProductVersion(fullPath, pszVersion, sizeof(pszVersion))) {
+                PushStringA(pszVersion);
+				return;
+            }
+		}
+
+		PushStringA("failed");
+    }
+
+	void __declspec(dllexport) IsX64OperatingSystem(HWND hwndParent, int string_size,
+        LPTSTR variables, stack_t **stacktop,
+        extra_parameters *extra, ...)
+    {
+        // note if you want parameters from the stack, pop them off in order.
+        // i.e. if you are called via exdll::myFunction file.dat read.txt
+        // calling popstring() the first time would give you file.dat,
+        // and the second time would give you read.txt. 
+        // you should empty the stack of your parameters, and ONLY your
+        // parameters.
+        EXDLL_INIT();
+     
+		pushint(isWOW64());
+    }
+
+	BOOL WINAPI DllMain(HINSTANCE hInst, ULONG ul_reason_for_call, LPVOID lpReserved)
+	{
+	  // g_hInstance = hInst;
+	  return TRUE;
+	}
+#if defined(__cplusplus)
+}
+#endif
